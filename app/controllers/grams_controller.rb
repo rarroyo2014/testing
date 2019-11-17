0468a7fc01d2808999c40cbf5a39f81d965ae179
Rebeca Arroyo <rarroyo2014@fau.edu>
2019-11-17 05:03:14 +0000

    destroying grams

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -3,6 +3,15 @@ class GramsController < ApplicationController
 
 
 
+  def destroy
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+    @gram.destroy
+    redirect_to root_path
+  end
+
+
+
   def update
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
