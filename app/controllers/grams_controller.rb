10fb21b9329a968a20cb170a5e74f91f26f63af4
Rebeca Arroyo <rarroyo2014@fau.edu>
2019-11-17 02:11:03 +0000

    adding a gram detail page

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -9,6 +9,12 @@ class GramsController < ApplicationController
   def index 
   end 
 
+  def show
+    @gram = Gram.find_by_id(params[:id])
+    if @gram.blank?
+      render plain: 'Not Found :(', status: :not_found
+    end
+  end
 
 
   def create 
